16b52b128b4a2ba186106fe03cfb6eb19c5fa8a8
HoangSon1908 <hzson1908@gmail.com>
2026-09-19 19:20:55 +0700

    Update max pressure values and gap settings

--- scanner.py
+++ scanner.py
@@ -81,7 +81,7 @@ DEFAULT_CONFIG = {
     # Cài đặt cho Highest
     "scan_highest": True,
     "h_min_p_no": 90.0,
-    "h_max_p_no": 99.7,
+    "h_max_p_no": 99.8,
     "h_filter_no": True,
     "h_gap_filter_enabled": True,
     "h_gap_top_k": 2,
@@ -90,10 +90,10 @@ DEFAULT_CONFIG = {
     # Cài đặt cho Lowest
     "scan_lowest": True,
     "l_min_p_no": 90.0,
-    "l_max_p_no": 99.7,
+    "l_max_p_no": 99.8,
     "l_filter_no": True,
     "l_gap_filter_enabled": True,
-    "l_gap_top_k": 2,
+    "l_gap_top_k": 1,
     "l_gap_value": 4,
 
     # Dùng chung
